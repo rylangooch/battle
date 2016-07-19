1583aaed2b59d34486d4650a8df6e36e944d5b6b
Michael <mjosephmiller.uk@gmail.com>
2016-07-19 15:33:55 +0100

    add feature to see HP for Player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require '../battleships/app.rb'
+require '../battle/app.rb'
 
 feature 'Enter names' do
   scenario 'submitting names' do
@@ -9,4 +9,14 @@ feature 'Enter names' do
     click_button 'Submit'
     expect(page).to have_content 'Will vs. Mike'
   end
+ end
+
+ feature 'View hit points'do
+   scenario 'see Player 2 hit points' do
+     visit('/')
+     fill_in :player_1_name, with: 'Will'
+     fill_in :player_2_name, with: 'Mike'
+     click_button 'Submit'
+     expect(page).to have_content 'Mike: 100HP'
+   end
  end
