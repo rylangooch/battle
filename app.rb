0367b50ed61a5fa316e16ff808675e5c792f85ec
Michael <mjosephmiller.uk@gmail.com>
2016-07-19 13:50:50 +0100

    enables user to enter player names

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+  get '/' do
     'Hello Battle!'
   end
 
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require '../battleships/app.rb'
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Will'
+    fill_in :player_2_name, with: 'Mike'
+    click_button 'Submit'
+    expect(page).to have_content 'Will vs. Mike'
+  end
+ end
