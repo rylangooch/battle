437b6ae8c67e2721478ca903bc4d95849c3dd7e9
William Osborne <wosborne@live.co.uk>
2016-07-19 15:54:03 +0100

    DRYed up code with web_helpers and hit_points files

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,20 +3,7 @@ require '../battle/app.rb'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Will'
-    fill_in :player_2_name, with: 'Mike'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Will vs. Mike'
   end
- end
-
- feature 'View hit points'do
-   scenario 'see Player 2 hit points' do
-     visit('/')
-     fill_in :player_1_name, with: 'Will'
-     fill_in :player_2_name, with: 'Mike'
-     click_button 'Submit'
-     expect(page).to have_content 'Mike: 100HP'
-   end
  end
